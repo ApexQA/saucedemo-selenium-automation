1693f1f84f30c118dd2dc8b09b52b9fb855fdced
muhamad-hamed <muhamaad.hamed@gmail.com>
2025-04-20 15:54:36 +0200

    use base test class with needed options

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,38 +1,18 @@
 package tests;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.LoginPage;
+import utilities.BaseTest;
 import utilities.ConfigReader;
 
 import java.time.Duration;
 
-public class LoginTests {
-    private WebDriver driver;
+public class LoginTests  extends BaseTest {
 
-    @BeforeMethod
-    public void setup() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--remote-allow-origins=*");
-
-        // Initialize the class-level 'driver' variable
-        driver = new ChromeDriver(options);
-        System.out.println("Driver initialized: " + (driver != null)); // Debugging log
-
-        driver.get(ConfigReader.getProperty("base.url"));
-    }
-
-    @Test(timeOut = 10000) // Test will fail if it takes longer than 10 seconds
+    @Test(timeOut = 10000)
     public void testValidLogin() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         LoginPage loginPage = new LoginPage(driver);
@@ -49,13 +29,5 @@ public class LoginTests {
         String expectedUrl = "https://www.saucedemo.com/inventory.html";
         String actualUrl = driver.getCurrentUrl();
         Assert.assertEquals(actualUrl, expectedUrl, "Login failed: URL mismatch");
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        // Close the browser
-        if (driver != null) {
-            driver.quit();
-        }
     }
 }
--- src/test/java/utilities/BaseTest.java
+++ src/test/java/utilities/BaseTest.java
@@ -3,6 +3,7 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -11,16 +12,28 @@ public class BaseTest {
 
     @BeforeMethod
     public void setup() {
-        WebDriverManager.chromedriver().setup(); // Automatic version management
-        driver = new ChromeDriver();
+        // Set up ChromeDriver using WebDriverManager
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+
+        // Initialize ChromeDriver with options
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
+        System.out.println("Browser initialized and navigated to: https://www.saucedemo.com/");
     }
 
     @AfterMethod
     public void tearDown() {
+        // Close the browser after each test
         if (driver != null) {
             driver.quit();
+            System.out.println("Browser closed.");
         }
     }
 }
